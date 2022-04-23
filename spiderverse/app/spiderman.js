01abd7b7a84aedddeb6b53736620175fb191b5f7
EinarDvls <launchx06534@innovaccion.mx>
2022-04-22 23:34:17 -0500

    Agregando pruebas solito

--- spiderverse/app/spiderman.js
+++ spiderverse/app/spiderman.js
@@ -1,7 +1,15 @@
 class Spiderman{
-    constructor(name){
-        this.name = name
+    constructor(name, age, actor, movies, studio){
+      this.name = name
+      this.age = age
+      this.actor = actor
+      this.movies = movies
+      this.studio = studio
     }
-}
-
-module.exports = Spiderman
+  
+    getInfo(){
+      return `Hey, I'm ${this.actor} from ${this.studio} studio`
+    }
+  }
+  
+  module.exports = Spiderman
--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -5,13 +5,17 @@ describe("Test Suite Dummy Description", () => {
 
       // Aqui escribimos el código que queremos usar tal cuál
       // Quiero poder instanciar un objeto Spiderman con esta información
-      const tomHolland = new Spiderman("Spiderman Sony", 25, "Tom Holland", 3, "Sony")
+      const tomHolland = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
 
       // Validamos que esté código funcione de la forma esperada
       expect(tomHolland.name).toBe("Spiderman Sony")
-      expect(tomHolland.age).toBe(25)
-      expect(tomHolland.actor).toBe("Tom Holland")
-      expect(tomHolland.movies).toBe(3)
+      expect(tomHolland.age).toBe(31)
+      expect(tomHolland.actor).toBe("Andrew Garfield")
+      expect(tomHolland.movies).toBe(2)
       expect(tomHolland.studio).toBe("Sony");
+    });
+    test('2) Use the method getInfo()', () => {
+      const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+      expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     });
   })
